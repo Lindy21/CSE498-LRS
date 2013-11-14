f0ceaa508e2dbeb649b7f797319b2c4162385ca9
Michael Quiroga <quiroga2@msu.edu>
2013-11-14 18:11:13 -0500

    jkl

--- lrs/views.py
+++ lrs/views.py
@@ -162,7 +162,7 @@ def about(request):
     return HttpResponse(req_process.stream_response_generator(lrs_data), mimetype="application/json", status=200)
 
 def testquiz(request):
-    return render_to_response('smart/Learning_Activity_Capture_Quiz_player.html', {}, context_instance=RequestContext(request))  
+    return render_to_response('smart/Learning_Activity_Capture_Quiz_player.html')  
 
 def actexample(request):
     return render_to_response('actexample.json', mimetype="application/json")
